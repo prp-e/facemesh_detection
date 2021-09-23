7dc5be6fcfc976f3d91cdd236573dcaf97484a42
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-09-23 12:11:38 +0330

    Static image detection done a little bit better

--- static_image.py
+++ static_image.py
@@ -5,7 +5,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
 
-image = cv2.imread('Lenna.png')
+image = cv2.imread('scarlett-johansson.jpg')
 
 with mp_face_mesh.FaceMesh(min_detection_confidence=.6) as face_mesh:
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -18,7 +18,13 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=.6) as face_mesh:
 
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(image=image, landmark_list=face_landmarks, connections=mp_face_mesh.FACEMESH_CONTOURS, landmark_drawing_spec=None)
+            #mp_drawing.draw_landmarks(image=image, landmark_list=face_landmarks, connections=mp_face_mesh.FACEMESH_CONTOURS, landmark_drawing_spec=None)
+
+            for id, landmark in enumerate(face_landmarks.landmark):
+                h, w, c = image.shape
+                cx, cy = int(landmark.x * w), int(landmark.y * h)
+                cv2.putText(image, str(id), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, .25, (0, 0, 255), 1)
+
 
 
 cv2.imshow('Static Image', image)
