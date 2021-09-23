bb907f794e861f11db1b9190c6784553e469abb2
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-09-23 11:44:39 +0330

    A quick fix in image name

--- static_image.py
+++ static_image.py
@@ -5,7 +5,20 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
 
-image = cv2.imread('Lenna.jpg')
+image = cv2.imread('Lenna.png')
+
+with mp_face_mesh.FaceMesh(min_detection_confidence=.6) as face_mesh:
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False 
+
+    results = face_mesh.process(image)
+
+    image.flags.writeable = True
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            mp_drawing.draw_landmarks(image=image, landmark_list=face_landmarks, connections=mp_face_mesh.FACEMESH_TESSELATION, landmark_drawing_spec=None)
+
 
 cv2.imshow('Static Image', image)
 cv2.waitKey(0)
