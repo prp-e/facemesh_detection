5c8b038ff1fd7a2f8973e0046d595a1853f5dae3
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-09-23 12:17:49 +0330

    It's now driving me crazy

--- static_image.py
+++ static_image.py
@@ -1,9 +1,11 @@
 import cv2
 import mediapipe as mp
+import time 
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
+
 
 image = cv2.imread('scarlett-johansson.jpg')
 
